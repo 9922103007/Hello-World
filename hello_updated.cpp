2a8b61fb4d437cb46d199075326e9621b99fd5c6
9922103007 <9922103007@mail.jiit.ac.in>
2024-07-24 15:37:41 +0530

    Add files via upload

---
+++ hello_updated.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cout<<"Enter 2 numbers a and b: ";
+    cin>>a>>b;
+    cout<<endl;
+    cout<<"Difference of "<<a<<"-"<<b<<" is: "<<a-b;
+
+    return 0;
+}
